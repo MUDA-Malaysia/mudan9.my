2b4ad7268d63e5967e338691bc4f8f455a55e66b
si3nloong <sianloong90@gmail.com>
2022-10-12 01:12:38 +0800

    chore: update UI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,10 +10,9 @@
     justify-content: center;
     align-items: center;
     color: #fff;
-    font-size: 6rem;
-    width: 100%;
+    font-weight: 900;
     height: 100%;
-
-    background: linear-gradient(to left, red 33.33%, black 33.33% 66.66%, yellow 66.66%);
+    font-size: 6rem;
+    // background: linear-gradient(to left, red 33.33%, black 33.33% 66.66%, yellow 66.66%);
   }
 </style>
