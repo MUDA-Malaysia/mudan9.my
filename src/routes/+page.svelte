14f6de374cae1534028fb4fcdbc57105cebd5220
si3nloong <sianloong90@gmail.com>
2022-10-18 13:24:50 +0800

    chore: add CTA on home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,39 @@
 <script lang="ts">
 </script>
 
-<h1 class="caption">MUDA N9</h1>
+<div class="container">
+  <h1>MUDA N9</h1>
+  <a class="join-btn" href="https://muda.my/sertai/">Join Us</a>
+</div>
 
 <style lang="scss">
+  .container {
+    display: flex;
+    height: 100%;
+    flex-direction: column;
+    justify-content: center;
+  }
+
   h1 {
     // position: fixed;
     display: flex;
     justify-content: center;
     align-items: center;
     color: #fff;
-    font-weight: 900;
-    height: 100%;
+    font-weight: 600;
+    // height: 100%;
     font-size: 6rem;
     // background: linear-gradient(to left, red 33.33%, black 33.33% 66.66%, yellow 66.66%);
+  }
+
+  .join-btn {
+    display: block;
+    color: #000;
+    background-color: #fbbe03;
+    height: 40px;
+    line-height: 40px;
+    font-size: 1.2rem;
+    text-transform: uppercase;
+    text-align: center;
   }
 </style>
